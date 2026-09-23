10016e5306f00a330448fea76e6f92b860e9561a
YONGLI-LJY\leeje <leejeayoung2022@gmail.com>
2026-09-23 13:26:30 +0900

    no message

--- source/ui.c
+++ source/ui.c
@@ -2200,9 +2200,9 @@ void encoder_page_change(u16 state)
                     Page_Change_UI(adminList[language]);
                 }else if(page_number == adminLanguage[language]){
                     adminLanguageText(state);
-                    write_dgus_vp(VP_LANGUAGE_FLAG, )
+                    write_dgus_vp(VP_LANGUAGE_FLAG,(u8*)&language,1)
                 }else if(page_number == adminCompany[language]){
-                    Page_Change_UI(adminList[language], (u8*)&language, 1);
+                    Page_Change_UI(adminList[language]);
                 }else if(page_number == adminEngineermod[language]){
                     EngineermodWork(state);
                 }else if(page_number == adminEngineermodS[language]){
